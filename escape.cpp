447fa157d9ca544c1ec8bda5e0882025704ee2db
hcy40735 <s1140447@dmhs.kh.edu.tw>
2026-05-22 10:55:33 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "1.幸福崴孟" << endl; 
+	cout << "2.幸福 崴孟" << endl; 
+	cout << "3.幸福\t崴孟" << endl; 
+	cout << "4.幸福\n崴孟" << endl; 
+	cout << "\"5.幸福崴孟\"" << endl; 
+	cout << "\'6.幸福崴孟\'" << endl; 
+	cout << "\\7.幸福崴孟\\" << endl; 
+	return 0;
+}
