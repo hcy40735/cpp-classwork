c4d37e7d8d05ac8fbcb7f27d9854169976d77a8b
hcy40735 <s1140447@dmhs.kh.edu.tw>
2026-06-12 10:48:59 +0800

    Create  array.cpp

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double catweight[6] = {11.2, 10.8, 10.4, 11.5, 12.0, 13.0 }
+    double average
+    for (int i = 0; i <- 5; i++) {
+    	sum = sum + cat weight[i]
+	}
+	cout << "cat平均體重: " << sum / 6; 
+    return 0;
+}
